14e9cb2380132b154f9a771e3dbbf9e1711a299e
Fabrizio Pece <1692167+fabriziopece@users.noreply.github.com>
2025-01-12 22:00:09 +0100

    - added website build

--- components.d.ts
+++ components.d.ts
@@ -9,11 +9,8 @@ declare module 'vue' {
   export interface GlobalComponents {
     AppBar: typeof import('./src/components/AppBar.vue')['default']
     Map: typeof import('./src/components/Map.vue')['default']
-    Map2: typeof import('./src/components/Map2.vue')['default']
-    MapPmTiles: typeof import('./src/components/MapPmTiles.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TutorialCard: typeof import('./src/components/TutorialCard.vue')['default']
-    TutorialPmTilesCard: typeof import('./src/components/TutorialPmTilesCard.vue')['default']
   }
 }
